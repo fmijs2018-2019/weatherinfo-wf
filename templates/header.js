b9e15ee6da9c8af8013e869d3be0e4a1cb2a494a
Daniela <daniela_post@abv.bg>
2019-04-02 09:29:16 +0300

    added swiper

---
+++ templates/header.js
@@ -0,0 +1,13 @@
+function getHeaderHtml() {
+	return	`<div class="ui secondary pointing menu">
+		<a class="item" href="home.html">
+			<b>WeatherInfo</b>
+		</a>
+		<a class="active teal item" href="home.html">
+			Home
+		</a>
+		<a class="item teal" href="about.html">
+			About
+		</a>
+	</div>`;
+}
---
+++ templates/swiper-item.js
@@ -0,0 +1,19 @@
+function getSwiperItemHtml() {
+	return `<div class="row">
+		<div class="col-12">
+			<div><img src="http://openweathermap.org/img/w/02d.png"></div>
+			<div>Monday</div>
+			<div>2 Apr</div>
+			<br>
+			<div class="ui horizontal label blue">Min: 3.9°С</div>
+			<div class="ui horizontal label red">Max: 13.2°С</div>
+			<div>Avarage temperature: <b>8.6°С</b></div>
+			<div>sky is clear</div>
+			<br>
+			<div>Wind: <b>3.97 m/s</b></div>
+			<div>Clouds: <b>0%</b></div>
+			<div>Pressure: <b>1004.51</b> hpa</div>
+			<div>Humidity: <b>56</b></div>
+		</div>
+	</div>`;
+}
